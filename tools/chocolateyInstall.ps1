7a7f1a547a96cc1327e4d592ee1d9c810b93131b
William Gathoye <william@gathoye.be>
2016-10-29 19:29:01 +0200

    Import files from Okhoshi

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'openvpn'
+$tools = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
+$silentArgs = '/S /SELECT_EASYRSA=1'
+$validExitCodes = @(0)
+
+Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\openvpn.cer'"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
