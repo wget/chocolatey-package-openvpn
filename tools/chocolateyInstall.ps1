4e3cc15db9ed1f5e6f48ed8f65807f4b0a8f982d
William Gathoye <william@gathoye.be>
2016-10-31 13:00:12 +0100

    Add checksums

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,6 +9,8 @@ $url64 = 'https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.12
 # https://github.com/OpenVPN/openvpn-build/blob/master/windows-nsis/openvpn.nsi#L102
 $silentArgs = '/S /SELECT_EASYRSA=1'
 $validExitCodes = @(0)
+$checksum = "0d6503300d2b9c9a1cb3b4e0af24528227c6b1d0e72c7b99ef070177fcfa1b1711a2a718df615e5be89d20376fe955481232c39848c5434fd63cd591f9d9711c"
+$checksum64="988870a8e8277282b5fb064379594a5fd618456676ad06d1be74311754cb270c62e411aba78db6b7be08a9d31ea4e66b313373a9a461894d57f99efe870f94ca"
 
 Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\openvpn.cer'"
 Install-ChocolateyPackage `
@@ -18,3 +20,7 @@ Install-ChocolateyPackage `
     -Url "$url" `
     -Url64bit "$url64" `
     -ValidExitCodes "$validExitCodes" `
+    -Checksum "$checksum" `
+    -ChecksumType 'sha512' `
+    -Checksum64 "$checksum64" `
+    -ChecksumType64 'sha512'
