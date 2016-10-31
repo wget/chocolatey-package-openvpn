cb339f66ea8356aa38d9c5b14ec0ee8000485bfb
William Gathoye <william@gathoye.be>
2016-10-31 12:59:57 +0100

    Add correct switches and their description + link of the sources

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,6 +3,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileType = 'exe'
 $url = 'https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.12-I602-i686.exe'
 $url64 = 'https://swupdate.openvpn.org/community/releases/openvpn-install-2.3.12-I602-x86_64.exe'
+# For a list of all silent arguments used
+# https://github.com/OpenVPN/openvpn-build/blob/master/windows-nsis/openvpn.nsi#L431
+# For their description
+# https://github.com/OpenVPN/openvpn-build/blob/master/windows-nsis/openvpn.nsi#L102
 $silentArgs = '/S /SELECT_EASYRSA=1'
 $validExitCodes = @(0)
 
