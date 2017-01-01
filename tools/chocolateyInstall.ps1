aac7b4e21bdf3c653057ee3f0795a1a9c94f676d
William Gathoye <william@gathoye.be>
2017-01-01 21:42:07 +0100

    Convert TAB to spaces

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -67,25 +67,25 @@ An object made of the following fields:
 - delayedStart (bool)
 #>
 function GetServiceProperties {
-	param (
-		[Parameter(Mandatory=$true)][string]$name
-	)
+    param (
+        [Parameter(Mandatory=$true)][string]$name
+    )
 
-	# Lets return our own object.
-	# src.: http://stackoverflow.com/a/12621314
-	$properties = "" | Select-Object -Property name,status,startupType,delayedStart
+    # Lets return our own object.
+    # src.: http://stackoverflow.com/a/12621314
+    $properties = "" | Select-Object -Property name,status,startupType,delayedStart
 
-	# Get-Service is not throwing an exception when the service name
-	# contains * (asterisks) and the service is not found. Prevent that.
-	if ($name -cmatch "\*") {
-		Write-Warning "Asterisks have been discarded from the service name '$name'"
-		$name = $name -Replace "\*",""
-	}
+    # Get-Service is not throwing an exception when the service name
+    # contains * (asterisks) and the service is not found. Prevent that.
+    if ($name -cmatch "\*") {
+        Write-Warning "Asterisks have been discarded from the service name '$name'"
+        $name = $name -Replace "\*",""
+    }
 
-	# The Get-Service Cmdlet returns a System.ServiceProcess.ServiceController
+    # The Get-Service Cmdlet returns a System.ServiceProcess.ServiceController
     # Get-Service throws an exception when the exact case insensitive service
     # is not found. Therefore, there is no need to make any further checks.
-	$service = Get-Service $name -ErrorAction Stop
+    $service = Get-Service $name -ErrorAction Stop
 
     # Correct to the exact service name
     if ($name -cnotmatch $service.Name) {
@@ -93,53 +93,53 @@ function GetServiceProperties {
     }
     $properties.name = $service.Name
 
-	# Get the service status. The Status property returns an enumeration
-	# ServiceControllerStatus src.: https://goo.gl/oq8Bbx
-	# This cannot be tested directly from CLI as the .NET assembly is not
-	# loaded, we get an exception
-	[array]$statusAvailable = [enum]::GetValues([System.ServiceProcess.ServiceControllerStatus])
-	if ($statusAvailable -notcontains "$($service.Status)") {
+    # Get the service status. The Status property returns an enumeration
+    # ServiceControllerStatus src.: https://goo.gl/oq8Bbx
+    # This cannot be tested directly from CLI as the .NET assembly is not
+    # loaded, we get an exception
+    [array]$statusAvailable = [enum]::GetValues([System.ServiceProcess.ServiceControllerStatus])
+    if ($statusAvailable -notcontains "$($service.Status)") {
         $errorString = "The status '$service.status' must be '"
-		$errorString += $statusAvailable -join "', '"
-		$errorString += "'"
-		throw "$errorString"
-	}
+        $errorString += $statusAvailable -join "', '"
+        $errorString += "'"
+        throw "$errorString"
+    }
 
     $properties.status = $service.Status
 
-	# The property StartType of the class System.ServiceProcess.ServiceController
-	# might not available in the .NET Framework when used with PowerShell 2.0
+    # The property StartType of the class System.ServiceProcess.ServiceController
+    # might not available in the .NET Framework when used with PowerShell 2.0
     # (cf. https://goo.gl/5NDtZJ). This property has been made available since
     # .NET 4.6.1 (src.: https://goo.gl/ZSvO7B).
-	# Since we cannot rely on this property, we need to find another solution.
-	# While WMI is widely available and working, let's parse the registry;
-	# later we will need an info exclusively storred in it.
+    # Since we cannot rely on this property, we need to find another solution.
+    # While WMI is widely available and working, let's parse the registry;
+    # later we will need an info exclusively storred in it.
 
-	# To list all the properties of an object:
-	# $services[0] | Get-ItemProperty
-	$service = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\$name
-	if (!$service) {
-		throw "The service '$name' was not found using the registry"
-	}
+    # To list all the properties of an object:
+    # $services[0] | Get-ItemProperty
+    $service = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\$name
+    if (!$service) {
+        throw "The service '$name' was not found using the registry"
+    }
 
-	# The values are the ones defined in
-	# [enum]::GetValues([System.ServiceProcess.ServiceStartMode])
-	switch ($service.Start) {
-		2 { $properties.startupType = "Automatic" }
-		3 { $properties.startupType = "Manual" }
-		4 { $properties.startupType = "Disabled" }
-		default { throw "The startup type is invalid" }
-	}
+    # The values are the ones defined in
+    # [enum]::GetValues([System.ServiceProcess.ServiceStartMode])
+    switch ($service.Start) {
+        2 { $properties.startupType = "Automatic" }
+        3 { $properties.startupType = "Manual" }
+        4 { $properties.startupType = "Disabled" }
+        default { throw "The startup type is invalid" }
+    }
 
-	# If the delayed flag is not set, there is no record DelayedAutoStart to the
-	# object.
-	if ($service.DelayedAutoStart) {
-		$properties.delayedStart = $true
-	} else {
-		$properties.delayedStart = $false
-	}
+    # If the delayed flag is not set, there is no record DelayedAutoStart to the
+    # object.
+    if ($service.DelayedAutoStart) {
+        $properties.delayedStart = $true
+    } else {
+        $properties.delayedStart = $false
+    }
 
-	return $properties
+    return $properties
 }
 
 <#
@@ -164,114 +164,114 @@ One of the following service startup type:
 - 'Disabled'
 #>
 function SetServiceProperties {
-	param (
-		# By default parameter are positional, this means the parameter name
-		# can be omitted, but needs to repect the order in which the arguments
-		# are declared, except if the PositionalBinding is set to false.
-		# src.: https://goo.gl/UpOU62
-		[Parameter(Mandatory=$true)][string]$name,
-		[Parameter(Mandatory=$true)][string]$status,
-		[Parameter(Mandatory=$true)][string]$startupType
-	)
+    param (
+        # By default parameter are positional, this means the parameter name
+        # can be omitted, but needs to repect the order in which the arguments
+        # are declared, except if the PositionalBinding is set to false.
+        # src.: https://goo.gl/UpOU62
+        [Parameter(Mandatory=$true)][string]$name,
+        [Parameter(Mandatory=$true)][string]$status,
+        [Parameter(Mandatory=$true)][string]$startupType
+    )
 
-	try {
-		$service = GetServiceProperties $name
-	} catch {
-		throw "The service '$name' cannot be found"
-	}
+    try {
+        $service = GetServiceProperties $name
+    } catch {
+        throw "The service '$name' cannot be found"
+    }
 
-	if ($env:ChocolateyEnvironmentDebug -eq 'true' -or
+    if ($env:ChocolateyEnvironmentDebug -eq 'true' -or
         $env:ChocolateyEnvironmentVerbose -eq 'true') {
-		Write-Verbose "Before SetServicesProperties:"
-		if ($service.delayedStart) {
-			Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type and delayed"
-		} else {
-			Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type"
-		}
-	}
+        Write-Verbose "Before SetServicesProperties:"
+        if ($service.delayedStart) {
+            Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type and delayed"
+        } else {
+            Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type"
+        }
+    }
 
-	# src.: https://goo.gl/oq8Bbx
-	[array]$statusAvailable = [enum]::GetValues([System.ServiceProcess.ServiceControllerStatus])
-	if ($statusAvailable -notcontains "$status") {
-		$errorString = "The status '$status' must be '"
-		$errorString += $statusAvailable -join "', '"
-		$errorString += "'"
-		throw "$errorString"
-	}
+    # src.: https://goo.gl/oq8Bbx
+    [array]$statusAvailable = [enum]::GetValues([System.ServiceProcess.ServiceControllerStatus])
+    if ($statusAvailable -notcontains "$status") {
+        $errorString = "The status '$status' must be '"
+        $errorString += $statusAvailable -join "', '"
+        $errorString += "'"
+        throw "$errorString"
+    }
 
-	if ($startupType -ne "Automatic (Delayed Start)" -and
-		$startupType -ne "Automatic" -and
-		$startupType -ne "Manual" -and
-		$startupType -ne "Disabled") {
-		throw "The startupType '$startupType' must either be 'Automatic (Delayed Start)', 'Automatic', 'Manual' or 'Disabled'"
-	}
+    if ($startupType -ne "Automatic (Delayed Start)" -and
+        $startupType -ne "Automatic" -and
+        $startupType -ne "Manual" -and
+        $startupType -ne "Disabled") {
+        throw "The startupType '$startupType' must either be 'Automatic (Delayed Start)', 'Automatic', 'Manual' or 'Disabled'"
+    }
 
-	# Set delayed auto start
-	if ($startupType -eq "Automatic (Delayed Start)") {
+    # Set delayed auto start
+    if ($startupType -eq "Automatic (Delayed Start)") {
 
-		# (src.: https://goo.gl/edhCxm and https://goo.gl/NyVXxM)
-		# Modifying the registry does not change the value in services.msc,
-		# using sc.exe does. sc.exe uses the Windows NT internal functions
-		# OpenServiceW and ChangeServiceConfigW. We could use it in PowerShell,
-		# but it would requires a C++ wrapper imported in C# code with
-		# DllImport, the same C# code imported in PowerShell. While this is
-		# doable, this is way slower than calling the sc utility directly.
-		# Set-ItemProperty -Path "Registry::HKLM\System\CurrentControlSet\Services\$($service.Name)" -Name DelayedAutostart -Value 1 -Type DWORD
-		# An .exe can be called directly but ensuring the exit code and
-		# stdout/stderr are properly redirected can only be checked with
-		# this code.
-		$psi = New-object System.Diagnostics.ProcessStartInfo
-		$psi.CreateNoWindow = $true
-		$psi.UseShellExecute = $false
-		$psi.RedirectStandardInput = $true
-		$psi.RedirectStandardOutput = $true
-		$psi.RedirectStandardError = $true
-		$process = New-Object System.Diagnostics.Process
-		$process.StartInfo = $psi
-		$psi.FileName = 'sc.exe'
-		$psi.Arguments = @("Config $($service.Name) Start= Delayed-Auto")
-		# The [void] casting is actually needed to avoid True or False to be displayed
-		# on stdout.
-		[void]$process.Start()
-		#PrintWhenVerbose $process.StandardOutput.ReadToEnd()
-		#PrintWhenVerbose $process.StandardError.ReadToEnd()
-		$process.WaitForExit()
-		if (!($process.ExitCode -eq 0)) {
-			throw "Unable to set the service '$($service.Name)' to a delayed autostart."
-		}
-	} else {
-		# Make sure the property DelayedAutostart is reset otherwise
-		# GetServiceProperties could report a service as Manual and delayed
-		# which is not possible.
-		Set-ItemProperty `
-		-Path "Registry::HKLM\System\CurrentControlSet\Services\$($service.Name)" `
-		-Name DelayedAutostart -Value 1 -Type DWORD -ErrorAction Stop
-	}
+        # (src.: https://goo.gl/edhCxm and https://goo.gl/NyVXxM)
+        # Modifying the registry does not change the value in services.msc,
+        # using sc.exe does. sc.exe uses the Windows NT internal functions
+        # OpenServiceW and ChangeServiceConfigW. We could use it in PowerShell,
+        # but it would requires a C++ wrapper imported in C# code with
+        # DllImport, the same C# code imported in PowerShell. While this is
+        # doable, this is way slower than calling the sc utility directly.
+        # Set-ItemProperty -Path "Registry::HKLM\System\CurrentControlSet\Services\$($service.Name)" -Name DelayedAutostart -Value 1 -Type DWORD
+        # An .exe can be called directly but ensuring the exit code and
+        # stdout/stderr are properly redirected can only be checked with
+        # this code.
+        $psi = New-object System.Diagnostics.ProcessStartInfo
+        $psi.CreateNoWindow = $true
+        $psi.UseShellExecute = $false
+        $psi.RedirectStandardInput = $true
+        $psi.RedirectStandardOutput = $true
+        $psi.RedirectStandardError = $true
+        $process = New-Object System.Diagnostics.Process
+        $process.StartInfo = $psi
+        $psi.FileName = 'sc.exe'
+        $psi.Arguments = @("Config $($service.Name) Start= Delayed-Auto")
+        # The [void] casting is actually needed to avoid True or False to be displayed
+        # on stdout.
+        [void]$process.Start()
+        #PrintWhenVerbose $process.StandardOutput.ReadToEnd()
+        #PrintWhenVerbose $process.StandardError.ReadToEnd()
+        $process.WaitForExit()
+        if (!($process.ExitCode -eq 0)) {
+            throw "Unable to set the service '$($service.Name)' to a delayed autostart."
+        }
+    } else {
+        # Make sure the property DelayedAutostart is reset otherwise
+        # GetServiceProperties could report a service as Manual and delayed
+        # which is not possible.
+        Set-ItemProperty `
+        -Path "Registry::HKLM\System\CurrentControlSet\Services\$($service.Name)" `
+        -Name DelayedAutostart -Value 1 -Type DWORD -ErrorAction Stop
+    }
 
-	# Cast "Automatic (Delayed Start)" to "Automatic" to have a valid name
-	if ($startupType -match "Automatic") {
-		$startupType = "Automatic"
-	}
+    # Cast "Automatic (Delayed Start)" to "Automatic" to have a valid name
+    if ($startupType -match "Automatic") {
+        $startupType = "Automatic"
+    }
 
-	# Set-Service cannot stop services properly and complains the service is
-	# dependent on other services, which seems to be wrong.
-	# src.: http://stackoverflow.com/a/39811972/3514658
-	if ($status -eq "Stopped") {
-		Stop-Service $service.Name -ErrorAction Stop
-	}
+    # Set-Service cannot stop services properly and complains the service is
+    # dependent on other services, which seems to be wrong.
+    # src.: http://stackoverflow.com/a/39811972/3514658
+    if ($status -eq "Stopped") {
+        Stop-Service $service.Name -ErrorAction Stop
+    }
 
-	Set-Service -Name $service.Name -StartupType $startupType -Status $status -ErrorAction Stop
+    Set-Service -Name $service.Name -StartupType $startupType -Status $status -ErrorAction Stop
 
-	if ($env:ChocolateyEnvironmentDebug -eq 'true' -or
+    if ($env:ChocolateyEnvironmentDebug -eq 'true' -or
         $env:ChocolateyEnvironmentVerbose -eq 'true') {
-		$service = GetServiceProperties $name
-		Write-Verbose "After SetServicesProperties:"
-		if ($service.delayedStart) {
-			Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type and delayed"
-		} else {
-			Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type"
-		}
-	}
+        $service = GetServiceProperties $name
+        Write-Verbose "After SetServicesProperties:"
+        if ($service.delayedStart) {
+            Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type and delayed"
+        } else {
+            Write-Verbose "Service '$($service.name)' now '$($service.status)', with '$($service.startupType)' startup type"
+        }
+    }
 }
 
 Write-Host "Downloading package installer..."
