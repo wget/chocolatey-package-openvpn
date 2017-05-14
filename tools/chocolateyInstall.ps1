30cb21f17b00f39b325ad7da2898dfee910b6824
William Gathoye <william@gathoye.be>
2017-05-14 13:05:41 +0200

    Bundle local version of the files

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -12,58 +12,55 @@ $url = 'https://build.openvpn.net/downloads/releases/openvpn-install-2.4.1-I601.
 $checksum = '83ac5500f9fc15c65bf8f2ca90f04c3043b7431fef763408c29746a7385b5a3ea313e11cf4fd274559c8cd9ba811cc6df49d2a84b94330f738fa31724edca4ba'
 $urlSig = 'https://build.openvpn.net/downloads/releases/openvpn-install-2.4.1-I601.exe.asc'
 $checksumSig = '8a250f7d77b96de64aa113bb9468f3d26d41f231ab3cb894bfacb8c809631db4227e8c5662d84512ae4fad2facf57ed8cb3e2ac3e6ed719f4d5b195fe43fa225'
-$pgpKey = "samuli_public_key.asc"
+$certFilename = "$toolsDir\openvpn.cer"
+$pgpKeyFileName = "$toolsDir\samuli_public_key.asc"
+$packageFileName = "$toolsDir\$($packageName)Install.$fileType"
+$sigFileName = "$toolsDir\$($packageName)Install.$fileType.asc"
 
 # Load custom functions
 . "$toolsDir\utils\utils.ps1"
 
-Write-Host "Downloading package installer..."
-$packageFileName = Get-ChocolateyWebFile `
-    -PackageName $packageName `
-    -FileFullPath $(Join-Path $(CreateTempDirPackageVersion) "$($packageName)Install.$fileType")`
-    -Url $url `
-    -Checksum $checksum `
+# If GPG has been just added, need to refresh to access to it from this session
+Update-SessionEnvironment
+
+Get-ChecksumValid `
+    -File "$sigFileName" `
+    -Checksum "$checksumSig" `
     -ChecksumType 'sha512'
 
-# Download signature and saving it as the original name
 # The GPG signature needs to have the same filename as the file checked but
 # with the .asc suffix, otherwise gpg reports it cannot verify the file with
 # the following message:
 # gpg: no signed data
 # gpg: can't hash datafile: No data
-Write-Host "Downloading package signature..."
-$sigFileName = Get-ChocolateyWebFile `
-    -PackageName $packageName `
-    -FileFullPath $(Join-Path $(CreateTempDirPackageVersion) "$($packageName)Install.$fileType.asc")`
-    -Url $urlSig `
-    -Checksum $checksumSig `
-    -ChecksumType 'sha512'
-
-# If GPG has been just added, need to refresh to access to it from this session
-Update-SessionEnvironment
-
 CheckPGPSignature `
-    -pgpKey "$toolsDir\$pgpKey" `
+    -pgpKey "$pgpKeyFileName" `
     -signatureFile "$sigFileName" `
     -file "$packageFileName"
 
 Write-Host "Adding OpenVPN to the Trusted Publishers (needed to have a silent install of the TAP driver)..."
-AddTrustedPublisherCertificate -file "$toolsDir\openvpn.cer"
+AddTrustedPublisherCertificate -file "$certFileName"
 
 Write-Host "Getting the state of the current OpenVPN service (if any)..."
 # Needed to reset the state of the Interactive service if upgrading from a
-# branch 2.4 or reinstalling a build from the branch 2.4
+# branch 2.4 and onwards or reinstalling a build from the branch 2.4
 try {
     $previousInteractiveService = GetServiceProperties "OpenVPNServiceInteractive"
 } catch {
     Write-Host "No previous OpenVPN interactive service detected."
 }
-# Needed for all cases 2.3 to 2.4 or 2.4 to 2.4.x and onwards
+# Even if 2.4.1 fixes reset of services. This is still needed for all cases 2.3
+# to 2.4 or 2.4 to 2.4.x and onwards.
 try {
     $previousService = GetServiceProperties "OpenVpnService"
 } catch {
     Write-Host "No previous OpenVPN service detected."
 }
+
+Get-ChecksumValid `
+    -File "$packageFileName" `
+    -Checksum "$checksum" `
+    -ChecksumType 'sha512'
 
 Install-ChocolateyInstallPackage `
     -PackageName $packageName `
@@ -93,4 +90,4 @@ if ($previousService) {
 }
 
 Write-Host "Removing OpenVPN from the Trusted Publishers..."
-RemoveTrustedPublisherCertificate -file "$toolsDir\openvpn.cer"
+RemoveTrustedPublisherCertificate -file "$certFileName"
--- update.ps1
+++ update.ps1
@@ -14,8 +14,24 @@ function global:au_SearchReplace {
 }
 
 function au_BeforeUpdate {
-    $Latest.checksum    = Get-RemoteChecksum -a sha512 $Latest.url
-    $Latest.checksumSig = Get-RemoteChecksum -a sha512 $Latest.urlSig
+    # We can't rely on Get-RemoteChecksum as we want to have the files locally
+    # as well and this function will download a local copy of the file, just to
+    # compute its hashes, then drop it. We can't rely completely on
+    # Get-RemoteFiles either as that function is only taking Latest URLs (x64
+    # and x32) into account. The signatures are not supported.
+    # src.: https://github.com/majkinetor/au/tree/master/AU/Public
+    $client = New-Object System.Net.WebClient
+    $toolsPath = Resolve-Path tools
+
+    $filePath = "$toolsPath/openvpnInstall.exe"
+    Write-Host "Downloading installer to '$filePath'..."
+    $client.DownloadFile($Latest.url, $filePath)
+    $Latest.checksum = Get-FileHash $filePath -Algorithm sha512 | % Hash
+
+    $filePath = "$toolsPath/openvpnInstall.exe.asc"
+    Write-Host "Downloading installer signature to '$filePath'..."
+    $client.DownloadFile($Latest.urlSig, $filePath)
+    $Latest.checksumSig = Get-FileHash $filePath -Algorithm sha512 | % Hash
 }
 
 function global:au_GetLatest {
@@ -30,7 +46,7 @@ function global:au_GetLatest {
     $url = $releases + "openvpn-install-" + $versionInstaller + ".exe"
     $urlSig = $url + ".asc"
 
-    @{ 
+    @{
         version = $version
         url     = $url
         urlSig  = $urlSig
